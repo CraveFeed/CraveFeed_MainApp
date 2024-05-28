af21d6adf4ab89a4e212d5756f475ebd2086d302
vibhor <vibgitcode27@gmail.com>
2024-05-28 13:22:30 +0530

    share food section in content route

--- src/app/(pages)/home/@content/page.tsx
+++ src/app/(pages)/home/@content/page.tsx
@@ -1,9 +1,15 @@
-import { Card } from "antd";
+import { Avatar, Card  , Flex} from "antd";
+import avatar from "../../../assets/avatar.jpg"
+import "../../../styles/content.css"
 
 export default function Content(){
     return (
-        <Card style={{backgroundColor : "#1B2730" , border : "none" , borderRadius : "20px" , color : "white", height : "400px" , width : "100%"}}>
-            This is bio section
-        </Card>
+        <Flex style={{backgroundColor : "#051017" , border : "none" , color : "white" , width : "100%"}}>
+            <Card style={{width : "100%" , backgroundColor : "#1B2730" , border : "none" , borderRadius : "20px" , height : "auto"}}>
+                <Flex>
+                    <Avatar size={50} src={avatar.src}/>
+                </Flex>
+            </Card>
+        </Flex>
     )
 }
